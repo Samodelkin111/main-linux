e5538ad947449f99912407d0d6c4d8d8dffcdc46
Samodelkin111 <100304544+Samodelkin111@users.noreply.github.com>
2024-12-01 20:17:29 +0300

    Update nfss.sh

--- nfss.sh
+++ nfss.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 sudo -i
 apt-get update;
-apt-get install -y nfs-common curl wget git nano mc
+apt-get install -y nfs-kernel-server curl wget git nano mc
 echo "NFS server setting"
 mkdir -p /srv/share/upload;
 chown -R nobody:nogroup /srv/share;
 chmod 0777 /srv/share/upload;
 cat <<EOF > 
 /etc/exports /srv/share 192.168.1.100/32(rw,sync,root_squash,no_subtree_check)
-EOF;
+EOF
 exportfs -r;
 exportfs -s;
 touch /srv/share/upload test-file;
