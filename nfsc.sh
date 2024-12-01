c5740d4f5b62e09202d038b409ef463ce22a8088
Samodelkin111 <100304544+Samodelkin111@users.noreply.github.com>
2024-12-01 21:47:13 +0300

    Update nfsc.sh

--- nfsc.sh
+++ nfsc.sh
@@ -1,11 +1,24 @@
 #! /bin/bash
 sudo -i;
 apt-get update;
-aptget install -y nfs-common curl wget git nano mc
+apt-get install -y nfs-common curl wget git nano mc
 echo "NFS cliet setting"
 echo "192.168.1.100:/srv/share/ /mnt nfs vers=3,noauto, x-systemd.automount 0 0" >> /etc/fstab;
-root@test-nfsc:~# systemctl daemon-reload;
-root@test-nfsc:~# systemctl restart remote-fs.target;
+systemctl daemon-reload;
+systemctl restart remote-fs.target;
+mount | grep mnt
+touch /mnt/upload/client_file;
+ls -l /mnt/upload;
+exit
+
+#! /bin/bash
+sudo -i;
+apt-get update;
+apt-get install -y nfs-common curl wget git nano mc
+echo "NFS cliet setting"
+echo "192.168.1.100:/srv/share/ /mnt nfs vers=3,noauto, x-systemd.automount 0 0" >> /etc/fstab;
+systemctl daemon-reload;
+systemctl restart remote-fs.target;
 mount | grep mnt
 touch /mnt/upload/client_file;
 ls -l /mnt/upload;
